3f2aab96dfc75f9e56ba1484d0bd243c6ba9f4ae
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2025-04-28 09:34:32 +0600

    edit

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -1,40 +1,37 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { useState } from 'react';
 
-import { MobileNav } from "@/components/mobile-nav";
-import { Logo } from "./logo";
-import Image from "next/image";
-import { X } from "lucide-react";
-import { Command } from "lucide-react";
-import { Button, buttonVariants } from "./ui/button";
-import { Menu } from "lucide-react";
+import { MobileNav } from '@/components/mobile-nav';
+import { Menu, X } from 'lucide-react';
+import { Logo } from './logo';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Button, buttonVariants } from './ui/button';
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+} from './ui/dropdown-menu';
 export function MainNav({ items, children }) {
 	const [showMobileMenu, setShowMobileMenu] = useState(false);
 
 	return (
 		<>
-			<div className="flex gap-6 lg:gap-10">
-				<Link href="/">
+			<div className='flex gap-6 lg:gap-10'>
+				<Link href='/'>
 					<Logo />
 				</Link>
 				{items?.length ? (
-					<nav className="hidden gap-6 lg:flex">
+					<nav className='hidden gap-6 lg:flex'>
 						{items?.map((item, index) => (
 							<Link
 								key={index}
-								href={item.disabled ? "#" : item.href}
+								href={item.disabled ? '#' : item.href}
 								className={cn(
-									"flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
+									'flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm'
 								)}>
 								{item.title}
 							</Link>
@@ -46,58 +43,58 @@ export function MainNav({ items, children }) {
 					<MobileNav items={items}>{children}</MobileNav>
 				)}
 			</div>
-			<nav className="flex items-center gap-3">
-				<div className="items-center gap-3 hidden lg:flex">
+			<nav className='flex items-center gap-3'>
+				<div className='items-center gap-3 hidden lg:flex'>
 					<Link
-						href="/login"
-						className={cn(buttonVariants({ size: "sm" }), "px-4")}>
+						href='/login'
+						className={cn(buttonVariants({ size: 'sm' }), 'px-4')}>
 						Login
 					</Link>
 					<DropdownMenu>
 						<DropdownMenuTrigger asChild>
-							<Button variant="outline" size="sm">
+							<Button variant='outline' size='sm'>
 								Register
 							</Button>
 						</DropdownMenuTrigger>
-						<DropdownMenuContent align="end" className="w-56 mt-4">
-							<DropdownMenuItem className="cursor-pointer">
-								<Link href="">Student</Link>
+						<DropdownMenuContent align='end' className='w-56 mt-4'>
+							<DropdownMenuItem className='cursor-pointer'>
+								<Link href='/register/student'>Student</Link>
 							</DropdownMenuItem>
-							<DropdownMenuItem className="cursor-pointer">
-								<Link href="">Instructor</Link>
+							<DropdownMenuItem className='cursor-pointer'>
+								<Link href='/register/instructor'>Instructor</Link>
 							</DropdownMenuItem>
 						</DropdownMenuContent>
 					</DropdownMenu>
 				</div>
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
-						<div className="cursor-pointer">
+						<div className='cursor-pointer'>
 							<Avatar>
 								<AvatarImage
-									src="https://github.com/shadcn.png"
-									alt="@shadcn"
+									src='https://github.com/shadcn.png'
+									alt='@shadcn'
 								/>
 								<AvatarFallback>CN</AvatarFallback>
 							</Avatar>
 						</div>
 					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end" className="w-56 mt-4">
-						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="account">Profile</Link>
+					<DropdownMenuContent align='end' className='w-56 mt-4'>
+						<DropdownMenuItem className='cursor-pointer' asChild>
+							<Link href='account'>Profile</Link>
 						</DropdownMenuItem>
-						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="account/enrolled-courses">My Courses</Link>
+						<DropdownMenuItem className='cursor-pointer' asChild>
+							<Link href='account/enrolled-courses'>My Courses</Link>
 						</DropdownMenuItem>
-						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="">Testimonials & Certificates</Link>
+						<DropdownMenuItem className='cursor-pointer' asChild>
+							<Link href=''>Testimonials & Certificates</Link>
 						</DropdownMenuItem>
-						<DropdownMenuItem className="cursor-pointer" asChild>
-							<Link href="">Logout</Link>
+						<DropdownMenuItem className='cursor-pointer' asChild>
+							<Link href=''>Logout</Link>
 						</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
 				<button
-					className="flex items-center space-x-2 lg:hidden"
+					className='flex items-center space-x-2 lg:hidden'
 					onClick={() => setShowMobileMenu(!showMobileMenu)}>
 					{showMobileMenu ? <X /> : <Menu />}
 				</button>
